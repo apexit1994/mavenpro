15fc8b64f71b4d47c1ef0dcee925c835efe4af44
Apexit Dhandav <adhandav@localhost.localdomain>
2019-04-25 12:23:37 +0530

    Initial Commit

---
+++ src/main/java/com/mkyong/hashing/App.java
@@ -0,0 +1,17 @@
+package com.mkyong.hashing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(add(2,5));
+    }
+
+    public static int add(int a, int b){
+        return a+b;
+    }
+}
